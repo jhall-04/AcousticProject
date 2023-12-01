fad54a3d4b94e6458d8a80341a5f78446e43675f
jhall-04 <133079520+jhall-04@users.noreply.github.com>
2023-12-01 20:34:50 +0000

    Implemented basic class structures

--- acoustic_controller.py
+++ acoustic_controller.py
@@ -1,8 +1,13 @@
 from pydub import AudioSegment
 
-wav_audio = AudioSegment.from_file("audio.wav", format="wav")
-raw_audio = AudioSegment.from_file("audio.wav", format="raw",
-                                   frame_rate=44100, channels=2, sample_width=2)
+class Controller:
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+    def fileConversion(file_path):
+        wav_audio = AudioSegment.from_file("audio.wav", format="wav")
+        raw_audio = AudioSegment.from_file("audio.wav", format="raw",
+                                        frame_rate=44100, channels=2, sample_width=2)
 
-wav_audio.export("audio1.mp3", format="mp3")
-raw_audio.export("audio2.mp3", format="mp3")
+        wav_audio.export("audio1.mp3", format="mp3")
+        raw_audio.export("audio2.mp3", format="mp3")
---
+++ acoustic_view.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+from tkinter import ttk
+
+class View(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)

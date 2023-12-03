b289967cce721430dfd441cdb07e285cf6c7fd6a
Aegis <aegis.katherine@gmail.com>
2023-12-03 16:08:48 -0500

    Modified: acoustic_controller to account for ffmpeg issues

--- acoustic_controller.py
+++ acoustic_controller.py
@@ -21,19 +21,19 @@ def check_format(audio_file):
     # gets the file type before converting if not a wav
     file_extension = pathlib.Path(audio_file).suffix
     if file_extension != ".wav":
+        #placeholder response
         print("There may be issues due to", file_extension, "not being in .wav format")
-        # temporarily commented out, this needs ffmpeg to install which has caused signifigant issues, reconviene later
-        # though honestly in looking at directions i am unsure if this calls for the file to be converted to .wav or
-        # if its checking for any corruption that may have happened during the conversion
+        # note further work requires use of ffmpeg install, which for windows is a bit of an effort.
         #--------------------------------------------------------------------------
-        # audio_converted = AudioSegment.from_file(audio_file, file_extension)
-        # audio_converted.export("Test_Audio.wav", format="wav")
+        audio_converted = AudioSegment.from_file(audio_file, file_extension)
+        audio_converted.export("Test_Audio.wav", format="wav")
 
 # test for cleaning test
 check_format(raw_audio)
 
 # test for acoustic statistics
-results = Statistics(raw_audio).get_length(raw_audio)
+results = Statistics(raw_audio)
+print(results.get_length(raw_audio))
 
 
 

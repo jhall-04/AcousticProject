6b08230227b1537910e1d6ad07ac035532d430ef
Aegis <aegis.katherine@gmail.com>
2023-12-03 18:09:04 -0500

    Modified: check_format function for cleaning now works as intended and will return a file guaranteed to be in the correct format

--- acoustic_controller.py
+++ acoustic_controller.py
@@ -1,39 +1,33 @@
-# Credit to the following library: https://github.com/jiaaro/pydub
 from acoustic_statistics import Statistics
 from pydub import AudioSegment
 import pathlib
 
+# --comment to be removed
 # replace the direct file call with the tkinter import once completed
-raw_audio = "Florida_Polytechnic_University_5.m4a"
+raw_audio = "Florida_Polytechnic_University_5.wav"
 
-#wav_audio = AudioSegment.from_file("audio.wav", format="wav")
-#raw_audio = AudioSegment.from_file("audio.wav", format="raw",
-                                   #frame_rate=44100, channels=2, sample_width=2)
-
-#wav_audio.export("audio1.mp3", format="mp3")
-#raw_audio.export("audio2.mp3", format="mp3")
 
 # --comment to be removed
-# needed to convert to wav so I did some placeholder work for the clean up
+# needed to convert to wav for the purposes of having a file to read for statistics so I went ahead and finished this
+# part of the cleanup code while I was at it
 
 
 def check_format(audio_file):
     # gets the file type before converting if not a wav
     file_extension = pathlib.Path(audio_file).suffix
     if file_extension != ".wav":
-        #placeholder response
-        print("There may be issues due to", file_extension, "not being in .wav format")
-        # note further work requires use of ffmpeg install, which for windows is a bit of an effort.
-        #--------------------------------------------------------------------------
-        audio_converted = AudioSegment.from_file(audio_file, file_extension)
-        audio_converted.export("Test_Audio.wav", format="wav")
+        audio_converted = AudioSegment.from_file(audio_file)
+        audio_converted.export("Florida_Polytechnic_University_5.wav", format="wav")
+        audio_file = "Florida_Polytechnic_University_5.wav"
+    return audio_file
 
-# test for cleaning test
-check_format(raw_audio)
+# temporary "main"
+# switches out the old audio clip for the new working one.
+audio_to_analyze = check_format(raw_audio)
 
 # test for acoustic statistics
-results = Statistics(raw_audio)
-print(results.get_length(raw_audio))
+results = Statistics(audio_to_analyze)
+print(results.get_length(audio_to_analyze))
 
 
 

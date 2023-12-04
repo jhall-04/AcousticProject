7aa2e0e8573a0c9cc9cc8d00fff227915a53eda2
jhall-04 <133079520+jhall-04@users.noreply.github.com>
2023-12-03 22:32:36 -0500

    Update acoustic_controller.py
    Replaced hard coded path with user inputted path

--- acoustic_controller.py
+++ acoustic_controller.py
@@ -18,8 +18,8 @@ class Controller:
         self.model = model
         self.view = view
     def fileConversion(file_path):
-        wav_audio = AudioSegment.from_file("audio.wav", format="wav")
-        raw_audio = AudioSegment.from_file("audio.wav", format="raw",
+        wav_audio = AudioSegment.from_file(file_path, format="wav")
+        raw_audio = AudioSegment.from_file(file_path, format="raw",
                                         frame_rate=44100, channels=2, sample_width=2)
 
         wav_audio.export("audio1.mp3", format="mp3")

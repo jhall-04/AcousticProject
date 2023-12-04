740491f47d55644379c4f06a1f1da62e18fb7078
jhall-04 <133079520+jhall-04@users.noreply.github.com>
2023-12-03 22:35:00 -0500

    Update acoustic_controller.py

--- acoustic_controller.py
+++ acoustic_controller.py
@@ -17,19 +17,12 @@ class Controller:
     def __init__(self, model, view):
         self.model = model
         self.view = view
-    def fileConversion(file_path):
-        wav_audio = AudioSegment.from_file(file_path, format="wav")
-        raw_audio = AudioSegment.from_file(file_path, format="raw",
-                                        frame_rate=44100, channels=2, sample_width=2)
-
-        wav_audio.export("audio1.mp3", format="mp3")
-        raw_audio.export("audio2.mp3", format="mp3")
 
     def check_format(self, audio_file):
         # gets the file type before converting if not a wav
         file_extension = pathlib.Path(audio_file).suffix
         if file_extension != ".wav":
             audio_converted = AudioSegment.from_file(audio_file)
-            audio_converted.export("Florida_Polytechnic_University_5.wav", format="wav")
-            audio_file = "Florida_Polytechnic_University_5.wav"
+            audio_converted.export("Clap.wav", format="wav")
+            audio_file = "Clap.wav"
         return audio_file
